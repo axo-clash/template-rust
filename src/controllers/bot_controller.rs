0842502a3e0c45bd6f1edcd3e6e587cd71c6ce15
Nathan Mittelette <nathan.mittelette@axopen.com>
2025-09-10 17:51:32 +0200

    fix: dto

--- src/controllers/bot_controller.rs
+++ src/controllers/bot_controller.rs
@@ -1,6 +1,7 @@
 use actix_web::{HttpResponse, Result, web};
 
 use crate::models::game_bot::GameBotDTO;
+use crate::models::play_response::PlayResponseDTO;
 use crate::services::bot_service::BotService;
 
 #[actix_web::get("/info")]
@@ -12,5 +13,5 @@ pub async fn get_bot_info() -> Result<HttpResponse> {
 #[actix_web::post("/play")]
 pub async fn play(game_state: web::Json<GameBotDTO>) -> Result<HttpResponse> {
     let action = BotService::play(&game_state);
-    Ok(HttpResponse::Ok().json(action))
+    Ok(HttpResponse::Ok().json(PlayResponseDTO { action }))
 }
